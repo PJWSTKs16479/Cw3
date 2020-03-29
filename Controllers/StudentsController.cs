40d3334dcef1b9190c949a1572f553f22035ccec
s16479 <s16479@pjwstk.edu.pl>
2020-03-29 14:19:31 +0200

    Third Commit

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cw3.DAL;
 using Cw3.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,11 +13,18 @@ namespace Cw3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
         [HttpGet]
 
-        public string GetStudent(string orderBy)
+        public IActionResult GetStudent(string orderBy)
         {
-            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+            return Ok(_dbService.GetStudents());
         }
 
         [HttpGet("{id}")]
